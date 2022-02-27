7ec831b142f918bd4c4801456e738a2d2daa4bfe
511 <fqd511@gmail.com>
2022-02-27 17:20:47 +0800

    chore: add rollup plugins ; update pack cofig

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,34 @@
 import ts from "rollup-plugin-ts";
 import del from "rollup-plugin-delete";
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: "index.ts",
-  output: { dir: "dist", format: "es", sourcemap: true },
+  output: [
+    {
+      file: './dist/util.umd.js',
+      format: 'umd',
+      name: 'util'
+    },
+    {
+      file: './dist/util.es.js',
+      format: 'es',
+      sourcemap: true
+    },
+    {
+      file: './dist/util.cjs.js',
+      format: 'cjs'
+    }
+  ],
   plugins: [
     ts({
       tsconfig: "tsconfig.json",
     }),
     del({ targets: "dist/*", runOnce: true }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    terser()
   ],
 };
